88d3a8542f9a92ca7a7d0750d03684e9721993e9
Matthew Merritt <matthew.merritt@yahoo.com>
2020-05-07 11:41:26 -0700

    Initial Commit

--- Example/LabelToolbar/ViewController.swift
+++ Example/LabelToolbar/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import LabelToolbar
 
 class ViewController: UIViewController {
+
+    let labelToolbar = LabelToolbar()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        toolbarItems = labelToolbar.items
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        labelToolbar.setAttributedText(for: .left, title: "Left Title: ", detail: "10:00")
+        labelToolbar.setAttributedText(for: .middle, title: "Middle Title: ", detail: "Click")
+        labelToolbar.setAttributedText(for: .right, title: "Right Title: ", detail: "25")
+
+        labelToolbar.middleButtonActionClosure = { label in
+            print("Label: \(label) clicked!")
+        }
     }
 
     override func didReceiveMemoryWarning() {
